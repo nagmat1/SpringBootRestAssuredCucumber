684b8c3c7ebe22b531603586d7dcd79c97d6cc67
nagmat1 <nagmat@gmail.com>
2024-11-16 23:10:23 -0800

    Second commit

--- src/test/java/com/nagmat/springweb/ProductrestapiApplicationTests.java
+++ src/test/java/com/nagmat/springweb/ProductrestapiApplicationTests.java
@@ -2,6 +2,16 @@ package com.nagmat.springweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features",
+		glue = "com.example.steps",
+		plugin = {"pretty", "html:target/cucumber-reports.html"},
+		monochrome = true)
 
 @SpringBootTest
 class ProductrestapiApplicationTests {
