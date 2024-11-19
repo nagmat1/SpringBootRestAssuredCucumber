dbaa650f05e43127ae350a6c4d70951062bd9df0
nagmat1 <nagmat@gmail.com>
2024-11-18 18:56:05 -0800

    5th Commit

--- src/test/java/com/nagmat/springweb/ProductApiSteps.java
+++ src/test/java/com/nagmat/springweb/ProductApiSteps.java
@@ -1,36 +1,39 @@
-package com.nagmat.steps;
+package com.nagmat.springweb;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@CucumberContextConfiguration
 public class ProductApiSteps {
 
     private Response response;
 
     @Given("I set the base URI for the API")
-    public void i_set_the_base_uri_for_the_api() {
-        RestAssured.baseURI = "http://localhost:8080/productapi/products";
+    public void iSetTheBaseUriForTheApi() {
+        RestAssured.baseURI = "http://localhost:8080/productapi/";
     }
 
     @When("I send a GET request to {string}")
-    public void i_send_a_get_request_to(String endpoint) {
+    public void iSendAGetRequestTo(String endpoint) {
         response = RestAssured.get(endpoint);
     }
 
     @Then("the response status code should be {int}")
-    public void the_response_status_code_should_be(int statusCode) {
+    public void theResponseStatusCodeShouldBe(int statusCode) {
+        System.out.println("Response: " + response.asString()+"Status code: "+statusCode);
         assertEquals(statusCode, response.getStatusCode());
     }
 
     @Then("the response should contain {string}")
-    public void the_response_should_contain(String key) {
-        response.then().body("$", hasKey(key));
+    public void theResponseShouldContain(String expectedValue) {
+        response.then().body("name", equalTo(expectedValue));
     }
+
 }
--- src/test/java/com/nagmat/springweb/ProductrestapiApplicationTests.java
+++ src/test/java/com/nagmat/springweb/ProductrestapiApplicationTests.java
@@ -12,40 +12,40 @@ import static org.junit.Assert.*;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/features",
-		glue = "com.nagmat.steps",
+		glue = "com.nagmat.springweb",
 		plugin = {"pretty", "html:target/cucumber-reports.html"},
 		monochrome = true)
 
-@SpringBootTest
+//@SpringBootTest
 class ProductrestapiApplicationTests {
 
-	@Test
-	public void testGetProduct() {
-		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
-		assertNotNull(product);
-		assertEquals("1+", product.getName());
-	}
-
-	@Test
-	public void testCreateProduct(){
-		RestTemplate restTemplate = new RestTemplate();
-		Product product = new Product();
-		product.setName("Samsung Galaxy");
-		product.setDescription("Samsung Galaxy S21");
-		product.setPrice(750);
-		Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products", product, Product.class);
-		assertNotNull(newProduct);
-		assertNotNull(newProduct.getId());
-		assertEquals("Samsung Galaxy", newProduct.getName());
-	}
+//	@Test
+//	public void testGetProduct() {
+//		RestTemplate restTemplate = new RestTemplate();
+//		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
+//		assertNotNull(product);
+//		assertEquals("1+", product.getName());
+//	}
+//
+//	@Test
+//	public void testCreateProduct(){
+//		RestTemplate restTemplate = new RestTemplate();
+//		Product product = new Product();
+//		product.setName("Samsung Galaxy");
+//		product.setDescription("Samsung Galaxy S21");
+//		product.setPrice(750);
+//		Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products", product, Product.class);
+//		assertNotNull(newProduct);
+//		assertNotNull(newProduct.getId());
+//		assertEquals("Samsung Galaxy", newProduct.getName());
+//	}
 	
-	@Test
-	public void testGetProduct() {
-		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
-		assertNotNull(product);
-		assertEquals("1+", product.getName());
-	}
+//	@Test
+//	public void testGetProduct() {
+//		RestTemplate restTemplate = new RestTemplate();
+//		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
+//		assertNotNull(product);
+//		assertEquals("1+", product.getName());
+//	}
 
 }
