b789e7c89cd6df708b1f81993e1ffd95d1286ff8
nagmat1 <nagmat@gmail.com>
2024-11-22 18:06:22 -0800

    5th commit

--- src/test/java/com/nagmat/springweb/ProductrestapiApplicationTests.java
+++ src/test/java/com/nagmat/springweb/ProductrestapiApplicationTests.java
@@ -2,6 +2,7 @@ package com.nagmat.springweb;
 
 import com.nagmat.springweb.entities.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -16,36 +17,40 @@ import static org.junit.Assert.*;
 		plugin = {"pretty", "html:target/cucumber-reports.html"},
 		monochrome = true)
 
-//@SpringBootTest
+@SpringBootTest
 class ProductrestapiApplicationTests {
 
-//	@Test
-//	public void testGetProduct() {
-//		RestTemplate restTemplate = new RestTemplate();
-//		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
-//		assertNotNull(product);
-//		assertEquals("1+", product.getName());
-//	}
-//
-//	@Test
-//	public void testCreateProduct(){
-//		RestTemplate restTemplate = new RestTemplate();
-//		Product product = new Product();
-//		product.setName("Samsung Galaxy");
-//		product.setDescription("Samsung Galaxy S21");
-//		product.setPrice(750);
-//		Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products", product, Product.class);
-//		assertNotNull(newProduct);
-//		assertNotNull(newProduct.getId());
-//		assertEquals("Samsung Galaxy", newProduct.getName());
-//	}
-	
-//	@Test
-//	public void testGetProduct() {
-//		RestTemplate restTemplate = new RestTemplate();
-//		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
-//		assertNotNull(product);
-//		assertEquals("1+", product.getName());
-//	}
+	@Value("${productrestapi.services.url}")
+	private String baseURL;
+
+	@Test
+	public void testGetProduct() {
+		System.out.println("Testing Get Product Method, the baseURL : "+baseURL);
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject(baseURL+"/1", Product.class);
+		assertNotNull(product);
+		assertEquals("1+", product.getName());
+	}
+
+	@Test
+	public void testCreateProduct(){
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = new Product();
+		product.setName("Samsung Galaxy");
+		product.setDescription("Samsung Galaxy S21");
+		product.setPrice(750);
+		Product newProduct = restTemplate.postForObject(baseURL, product, Product.class);
+		assertNotNull(newProduct);
+		assertNotNull(newProduct.getId());
+		assertEquals("Samsung Galaxy", newProduct.getName());
+	}
+
+	@Test
+	public void testUpdateProduct() {
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject(baseURL +"/1", Product.class);
+		product.setPrice(1400);
+		restTemplate.put(baseURL, product);
+	}
 
 }
